1f3f843a8752bde1762b085cef04fa11bd9c4dc0
Eldringrg <grg.aldrin@ymail.com>
2023-03-10 00:27:11 +0545

    just a little

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
